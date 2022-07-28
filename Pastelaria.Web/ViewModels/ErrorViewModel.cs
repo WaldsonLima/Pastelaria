6e51a4afbce96cd9e7bbbc3ba8f9d96cd0862504
Waldson Lima <waldsonh@gmail.com>
2022-07-28 14:19:11 -0300

    começando configs iniciais

---
+++ Pastelaria.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Pastelaria.Web.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
